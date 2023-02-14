94cf61db10e10ca1b14ffc06c11b414d612f2e12
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-02-14 08:47:42 +0000

    Add Kotlin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,22 @@
 import net.twisterrob.gradle.doNotNagAbout
 
+plugins {
+	alias(libs.plugins.kotlin)
+}
+
+kotlin {
+	jvmToolchain(libs.versions.java.get().toInt()) // TODO Kotlin versions
+}
+
+repositories {
+	mavenCentral()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach { 
+	compilerOptions.allWarningsAsErrors.set(true)
+	compilerOptions.verbose.set(true)
+}
+
 val gradleVersion: String = GradleVersion.current().version
 
 // TODEL Gradle sync in IDEA 2022.3.1: https://youtrack.jetbrains.com/issue/IDEA-306975
