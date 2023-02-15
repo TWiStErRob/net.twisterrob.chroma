c7174d8ffeda7e56548938f865b0da097e0e3628
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-02-15 11:20:59 +0000

    Listen for actions

--- idea/src/main/kotlin/net/twisterrob/chroma/intellij/shortcuts/ChromaListenerRegistrar.kt
+++ idea/src/main/kotlin/net/twisterrob/chroma/intellij/shortcuts/ChromaListenerRegistrar.kt
@@ -3,8 +3,14 @@ package net.twisterrob.chroma.intellij.shortcuts
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.plugins.DynamicPluginListener
 import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.AnActionResult
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.Shortcut
+import com.intellij.openapi.actionSystem.ex.AnActionListener
 
-class ChromaListenerRegistrar : AppLifecycleListener, DynamicPluginListener {
+class ChromaListenerRegistrar : AppLifecycleListener, DynamicPluginListener, AnActionListener {
 
 	override fun appFrameCreated(commandLineArgs: MutableList<String>) {
 		ChromaService.getInstance().ensureStarted()
@@ -22,7 +28,22 @@ class ChromaListenerRegistrar : AppLifecycleListener, DynamicPluginListener {
 
 	override fun pluginUnloaded(pluginDescriptor: IdeaPluginDescriptor, isUpdate: Boolean) {
 		if (pluginDescriptor.pluginId.idString == "net.twisterrob.chroma.intellij.shortcuts") {
-			ChromaService.getInstance().ensureStopped()
+//			ChromaService.getInstance().ensureStopped()
 		}
+	}
+
+	override fun beforeActionPerformed(action: AnAction, event: AnActionEvent) {
+	}
+
+	override fun afterActionPerformed(action: AnAction, event: AnActionEvent, result: AnActionResult) {
+	}
+
+	override fun beforeShortcutTriggered(shortcut: Shortcut, actions: MutableList<AnAction>, dataContext: DataContext) {
+	}
+
+	override fun beforeEditorTyping(c: Char, dataContext: DataContext) {
+	}
+
+	override fun afterEditorTyping(c: Char, dataContext: DataContext) {
 	}
 }
