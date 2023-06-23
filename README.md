8df3bf93ceee60101e836ce8c65d5f14e29dc456
Róbert Papp <papp.robert.s@gmail.com>
2023-06-23 11:43:50 +0100

    Upgrade dependencies / libraries / plugins (#8)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # IntelliJ Plugin "Show Shortcuts with Razer Chroma"
+
+## Running
+
+See [IDEA Run configurations](.idea/runConfigurations) folder.
+
+`:idea:runIde` is the main project testing point.
+
+Others are for playing around with the Razer Chroma SDK.
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+import net.twisterrob.gradle.doNotNagAbout
 import net.twisterrob.gradle.settings.enableFeaturePreviewQuietly
 
 // TODEL https://github.com/gradle/gradle/issues/18971
@@ -5,19 +6,11 @@ rootProject.name = "net-twisterrob-chroma"
 
 pluginManagement {
 	includeBuild("gradle/plugins")
-	resolutionStrategy {
-		eachPlugin {
-			when (requested.id.id) {
-				"net.twisterrob.settings" -> {
-					useModule("net.twisterrob.gradle:twister-convention-settings:${requested.version}")
-				}
-			}
-		}
-	}
 }
 
 plugins {
-	id("net.twisterrob.settings") version "0.15"
+	id("net.twisterrob.gradle.plugin.settings") version "0.15.1"
+	id("org.gradle.toolchains.foojay-resolver-convention") version("0.5.0")
 }
 
 dependencyResolutionManagement {
@@ -30,3 +23,25 @@ enableFeaturePreviewQuietly("TYPESAFE_PROJECT_ACCESSORS", "Type-safe project acc
 
 include(":idea")
 include(":razer")
+
+val gradleVersion: String = GradleVersion.current().version
+
+// TODEL Gradle sync in IDEA 2022.3.1: https://youtrack.jetbrains.com/issue/IDEA-306975
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The AbstractArchiveTask.archivePath property has been deprecated. " +
+			"This is scheduled to be removed in Gradle 9.0. " +
+			"Please use the archiveFile property instead. " +
+			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
+	"at org.jetbrains.plugins.gradle.tooling.builder.ExternalProjectBuilderImpl\$_getSourceSets_closure"
+)
+
+// TODEL Gradle sync in IDEA 2022.3.1: https://youtrack.jetbrains.com/issue/IDEA-306975
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The AbstractArchiveTask.archivePath property has been deprecated. " +
+			"This is scheduled to be removed in Gradle 9.0. " +
+			"Please use the archiveFile property instead. " +
+			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
+	"at org.jetbrains.plugins.gradle.tooling.util.SourceSetCachedFinder.createArtifactsMap"
+)
