928f4b4a79df43e967d03fa3e99b4c07bf5a0782
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-06 11:01:18 +0100

    Ignore IDEA sync warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -45,3 +45,37 @@ doNotNagAbout(
 			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
 	"at org.jetbrains.plugins.gradle.tooling.util.SourceSetCachedFinder.createArtifactsMap"
 )
+
+// TODEL Gradle 8.2 sync in IDEA 2023.1 https://youtrack.jetbrains.com/issue/IDEA-320266.
+@Suppress("MaxLineLength", "StringLiteralDuplication")
+if ((System.getProperty("idea.version") ?: "") < "2023.2") {
+	// There are ton of warnings, ignoring them all by their class names in one suppression.
+	doNotNagAbout(
+		Regex(
+			"^" +
+					"(" +
+					Regex.escape("The Project.getConvention() method has been deprecated. ") +
+					"|" +
+					Regex.escape("The org.gradle.api.plugins.Convention type has been deprecated. ") +
+					"|" +
+					Regex.escape("The org.gradle.api.plugins.JavaPluginConvention type has been deprecated. ") +
+					")" +
+					Regex.escape("This is scheduled to be removed in Gradle 9.0. ") +
+					Regex.escape("Consult the upgrading guide for further information: ") +
+					Regex.escape("https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#") +
+					".*" +
+					"(" +
+					Regex.escape("at org.jetbrains.kotlin.idea.gradleTooling.KotlinTasksPropertyUtilsKt.") +
+					"|" +
+					Regex.escape("at org.jetbrains.plugins.gradle.tooling.util.JavaPluginUtil.") +
+					"|" +
+					Regex.escape("at org.jetbrains.plugins.gradle.tooling.builder.ExternalProjectBuilderImpl.") +
+					"|" +
+					Regex.escape("at org.jetbrains.plugins.gradle.tooling.builder.ProjectExtensionsDataBuilderImpl.") +
+					")" +
+					".*$"
+		)
+	)
+} else {
+	logger.warn("IDEA version changed, please review hack.")
+}
