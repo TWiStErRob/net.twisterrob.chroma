26f00f113095fee23033556c5748cdbd60214d5c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-11-28 22:46:04 +0000

    Correctly hide message: Type-safe project accessors is an incubating feature.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ dependencyResolutionManagement {
 	//repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
 
-enableFeaturePreviewQuietly("TYPESAFE_PROJECT_ACCESSORS", "Type-safe project accessors is an incubating feature.")
+enableFeaturePreviewQuietly("TYPESAFE_PROJECT_ACCESSORS", "Type-safe project accessors")
 
 include(":idea")
 include(":razer")
